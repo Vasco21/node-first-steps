060f4d2c7dec0e1acbb746715ba8dbeb42c25f79
Vasco Eti <vascoeti123@gmail.com>
2021-07-20 13:56:55 +0200

    update

---
+++ greet_xola.js
@@ -0,0 +1,18 @@
+const chalk = require('chalk');
+var figlet = require('figlet');
+//import the greet module that is in the current folder
+const greet = require('./greet');
+
+const styledMessage = chalk.bgGreen.black(greet('Xola'));
+console.log(styledMessage)
+
+figlet('hello Vasco', function(err, data){
+    if(err){
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+})
+const styledMessage1 = chalk.bgRedBright.blue(greet('Vasco'));
+console.log(styledMessage1);
